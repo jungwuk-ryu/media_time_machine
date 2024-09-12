11a0544a9608b1bbc4e5b9bb67c1a9505c2a8e92
jungwuk <vojougae35@gmail.com>
2024-09-12 14:25:49 +0900

    feat: 미디어 선택기 태블릿 (큰 화면) 대응

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:photo_timemachine/app/routes/app_pages.dart';
@@ -12,19 +14,31 @@ class HomeController extends GetxController {
     // 사진 라이브러리 접근 권한 요청
     final PermissionState permission =
         await PhotoManager.requestPermissionExtend();
-    if (permission.isAuth) {
+
+    if (permission.isAuth) { // 권한 OK
+      int pageSize = 80;
+      // 최소 1, 최대 8
+      int gridCount = max(1, min(8, MediaQuery.of(Get.context!).size.width ~/ 90));
+
+      // pageSize % gridCount는 0 이어야 함
+      while (pageSize % gridCount != 0) {
+        gridCount --;
+      }
+
       final List<AssetEntity>? photos = await AssetPicker.pickAssets(
           Get.context!,
-          pickerConfig: const AssetPickerConfig(
+          pickerConfig: AssetPickerConfig(
               specialPickerType: SpecialPickerType.noPreview,
               maxAssets: 100000,
               themeColor: Colors.lightBlueAccent,
+              pageSize: pageSize,
+              gridCount: gridCount,
               sortPathsByModifiedDate: true));
 
       if (photos != null && photos.isNotEmpty) {
         Get.toNamed(Routes.TIMESELECTION, arguments: photos);
       }
-    } else {
+    } else { // 권한 없음
       Get.snackbar(
         "Permission Denied",
         "Permission not granted. Please enable it in settings.",
